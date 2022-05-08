a5ad58a8cbadcb37997266db8b7fb9bf3a9f34aa
SpezialK-dev <spezcialK@protonmail.com>
2022-05-08 18:59:00 +0200

    moved code to funktion to repeat itself
    still working on fixing the code

--- linearCongruenttialGen.hs
+++ linearCongruenttialGen.hs
@@ -1,18 +1,24 @@
 --this is the the File for the LInear congurential generator
-
 --defining the constants these will define how strong this algo is!!
 let a = 16807
 let c = 0
 let m = 2147483647
+--the algo signatur
+algo :: Int -> Int -> Int
 
---taking the user seed for X this would need to be replaced by something unpredictable and not the user input
-xString <- getLine
-let x = read xString :: Integer
+--the actuall algo 
+algo n prevNumb = 
+    do 
+        let out = a * prevNumb + c  `mod` m
+        algo(n-1, out)
 
--- how many cicles you want
-iString <- getLine
-let i = read xString :: Integer
+main = do
 
---the actuall algo
---todo make this loop i times 
-let out = a*x + c  `mod` m 
+    --taking the user seed for X this would need to be replaced by something unpredictable and not the user input
+    xString <- getLine
+    let x = read xString :: Int
+
+    -- how many cicles you want
+    iString <- getLine
+    let i = read xString :: Int
+    print(algo x i)
