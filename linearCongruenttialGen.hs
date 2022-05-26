a5fb4a83a785c0bf04573b21a61a6483d673b702
SpezialK-dev <spezcialK@protonmail.com>
2022-05-26 21:52:40 +0200

    fixed indentation on some parts

--- linearCongruenttialGen.hs
+++ linearCongruenttialGen.hs
@@ -1,20 +1,19 @@
 --this is the the File for the LInear congurential generator
 --defining the constants these will define how strong this algo is!!
-let a = 16807
-let c = 0
-let m = 2147483647
+a = 16807
+c = 0
+m = 2147483647
 
 algo :: Int -> Int 
-| n == 0 = 
-| a * algo(n-1, out) + c  `mod` m
+    | n == 0
+    out = a * algo(n-1, out) + c  `mod` m
 
-main = do
+--taking the user seed for X this would need to be replaced by something unpredictable and not the user input
 
-    --taking the user seed for X this would need to be replaced by something unpredictable and not the user input
-    xString <- getLine
-    let x = read xString :: Int
+xString <- getLine
+let x = read xString :: Int
+-- how many cicles you want
 
-    -- how many cicles you want
-    iString <- getLine
-    let i = read xString :: Int
-    print(algo x i)
+iString <- getLine
+let i = read xString :: Int
+print(algo x i)
